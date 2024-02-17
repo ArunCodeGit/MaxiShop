2001ed3885c93cfca79f3ad926a4c69d57577dcd
Arunkumar <arunkumarjayam8@gmail.com>
2024-02-17 23:37:40 +0530

    Initial commit for maxi shop sample project practising Web API.

---
+++ MaxiShop/Controllers/CategoryController.cs
@@ -0,0 +1,97 @@
+﻿using MaxiShop.Domain.Models;
+using MaxiShop.Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaxiShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var Categories =  _dbContext.Category.ToList();
+            return Ok(Categories);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        [Route("Details")]
+        public ActionResult Get(int id)
+        {
+            var Category = _dbContext.Category.FirstOrDefault(x=>x.Id == id);
+
+            if(Category==null)
+            {
+                return NotFound($"{id} Category not found.");
+            }
+
+            return Ok(Category);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult Create([FromBody]Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.Category.Add(category);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut]
+        public ActionResult Update([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.Category.Update(category);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            if (id==0)
+            {
+                return BadRequest();
+            }
+
+            var category = _dbContext.Category.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Category.Remove(category);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
