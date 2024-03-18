a24a7ed73250d4a6ea9d697f6dd373fcec4abb6b
Arunkumar <arunkumarjayam8@gmail.com>
2024-03-18 23:13:02 +0530

    Created brand controller and get all brand action method

--- MaxiShop/Controllers/BrandController.cs
+++ MaxiShop/Controllers/BrandController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MaxiShop.Application.ApplicationConstants;
+using MaxiShop.Application.Common;
+using MaxiShop.Application.Services.Interfaces;
+using MaxiShop.Domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace MaxiShop.Controllers
 {
@@ -7,6 +12,32 @@ namespace MaxiShop.Controllers
     [ApiController]
     public class BrandController : ControllerBase
     {
+        private readonly IBrandService _brandService;
+        protected readonly APIResponse _response;
 
+        public BrandController(IBrandService brandService)
+        {
+            _brandService = brandService;
+            _response = new APIResponse();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var brands = _brandService.GetAllAsync();
+                _response.statusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = brands;
+            }
+            catch (Exception)
+            {
+                _response.statusCode = HttpStatusCode.InternalServerError;
+                _response.AddErrors(CommonMessage.SystemError);
+            }
+            return Ok(_response);
+        }
     }
 }
