1540e066b942084e1bbf762fe97b48503aff10fb
Arunkumar <arunkumarjayam8@gmail.com>
2024-03-24 07:18:45 +0530

    Created GetAllProduct, Get By ID and Create method inside of new product controller.

---
+++ MaxiShop/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using MaxiShop.Application.ApplicationConstants;
+using MaxiShop.Application.Common;
+using MaxiShop.Application.DTO.Brand;
+using MaxiShop.Application.DTO.Product;
+using MaxiShop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MaxiShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        protected readonly APIResponse _response;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+            _response = new APIResponse();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var brands = await _productService.GetAllAsync();
+                _response.statusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = brands;
+            }
+            catch (Exception)
+            {
+                _response.statusCode = HttpStatusCode.InternalServerError;
+                _response.AddErrors(CommonMessage.SystemError);
+            }
+            return Ok(_response);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        [Route("Details")]
+        public async Task<ActionResult<APIResponse>> Get(int id)
+        {
+            try
+            {
+                var brand = await _productService.GetByIdAsync(id);
+
+                if(brand == null)
+                {
+                    _response.statusCode = HttpStatusCode.NotFound;
+                    _response.DisplayMessage = CommonMessage.RecordNotFound;
+
+                    return Ok(_response);
+                }
+
+                _response.IsSuccess = true;
+                _response.Result = brand;
+                _response.statusCode = HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                _response.statusCode = HttpStatusCode.InternalServerError;
+                _response.AddErrors(CommonMessage.SystemError);
+            }
+            return Ok(_response);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult<APIResponse>> Create(CreateProductDto dto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    _response.statusCode = HttpStatusCode.BadGateway;
+                    _response.AddErrors(ModelState.ToString());
+                    _response.DisplayMessage = CommonMessage.CreateOperationFailed;
+                    return Ok(_response);
+                }
+
+                var brand = await _productService.CreateAsync(dto);
+
+                _response.statusCode = HttpStatusCode.OK;
+                _response.DisplayMessage = CommonMessage.CreateOperationSuccess;
+                _response.IsSuccess = true;
+                _response.Result = brand;
+            }
+            catch (Exception)
+            {
+                _response.statusCode = HttpStatusCode.InternalServerError;
+                _response.AddErrors(CommonMessage.SystemError);
+                _response.DisplayMessage = CommonMessage.CreateOperationFailed;
+            }
+            return Ok(_response);
+        }
+    }
+}
--- MaxiShop/obj/Debug/net8.0/MaxiShop.AssemblyInfo.cs
+++ MaxiShop/obj/Debug/net8.0/MaxiShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaxiShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59f2fa0941922b2ccf014bb21920c82f244a301")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d7abd2704e3edd4f1fc92d362ca9c3ab882389")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaxiShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaxiShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
